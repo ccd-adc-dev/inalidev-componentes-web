9502b84a0d6e175262dbfa6b56efd0d3486eb8c5
ledautopica <vane.multimedia@gmail.com>
2019-10-12 18:30:34 -0500

    cambie color al background pero no veo el cambio

--- src/componentes/Glosario/Glosario.svelte
+++ src/componentes/Glosario/Glosario.svelte
@@ -9,24 +9,25 @@ dl {
     grid-template-columns: 1fr 1fr;
 }
 
+
 </style>
+
 <svelte:head>
   <title>Glosario INALI</title>
 </svelte:head>
 
 
-<Lateral>
+<Lateral backgroundColor="rgba (70,93,114,0.85)">
 
   <h1>Glosario</h1>
 
   <dl>
 
     <dt>
-        Familia lingüística..................................
+        Concepto
     </dt>
     <dd>
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorem accusantium voluptatibus, dolore obcaecati ipsam ratione molestiae ab eius ullam, hic officia? Voluptatem, commodi nobis.</p>
-        <p>Porro atque amet architecto odit, fugit magni cumque, id ad beatae delectus odio itaque doloribus, nihil nostrum tempora placeat neque dignissimos? Magni, voluptates repellendus.</p>
+        <p> Definición</p>
     </dd>
 
     <dt>
